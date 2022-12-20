73dced9a62b9ced09f4e5d08648c75b43ea14c9b
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-20 19:58:25 +0300

    added write txt

--- SeminarCriateFileXmlJson/src/WriteFileXml.java
+++ SeminarCriateFileXmlJson/src/WriteFileXml.java
@@ -1,6 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WriteFileXml extends WriteFile{
     @Override
     public void writeFileForClass()  {
+        try (FileWriter file = new FileWriter("markClass.xml")) {
 
+            String text = "Иван Иванов=4.8\n" +
+                    "Мария Кузнецова=5.0\n" +
+                    "Степан Кузьмин=3.6";
+            file.write(text);
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+
+        }
     }
 }
