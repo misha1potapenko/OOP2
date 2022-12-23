179a59e0194df247da41ad1164d3ed5dd0c5f739
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-23 22:19:53 +0300

    added iterator marker

--- HomeworkIterator/src/Group.java
+++ HomeworkIterator/src/Group.java
@@ -1,19 +1,27 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class Group {
+public class Group implements Iterable<Students> {
     private List<Students> students;
 
-    public List<Students> getStudentsGroup() {
-        return students;
+    public Group(List<Students> students) {
+
+        this.students = students;
     }
 
-    public Group(List<Students> studentsGroup) {
-        this.students = studentsGroup;
+
+    public int getSize(){
+        return students.size();
     }
 
     public Students get (int index){
         return students.get(index);
+    }
+
+    @Override
+    public Iterator<Students> iterator() {
+        return new GroupListIterator(this);
     }
 }
 
--- HomeworkIterator/src/GroupListIterator.java
+++ HomeworkIterator/src/GroupListIterator.java
@@ -1,21 +1,21 @@
 import java.util.Iterator;
 
 public class GroupListIterator implements Iterator<Students> {
-    private  int index = 0;
+    private  int index = -1;
+
+    private Group groupList;
 
     public GroupListIterator(Group groupList) {
         this.groupList = groupList;
     }
 
-    private Group groupList;
-
     @Override
     public boolean hasNext() {
-        return index < groupList.getStudentsGroup().size();
+        return index < groupList.getSize();
     }
 
     @Override
     public Students next() {
-        return groupList.getStudentsGroup().get(index++);
+        return groupList.get(index++);
     }
 }
