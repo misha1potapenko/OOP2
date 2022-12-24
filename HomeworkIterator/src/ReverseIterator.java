954b93a56cc4fef5a84940f516e11a285cafc5fd
misha1potapenko <misha1potapenko@yandex.ru>
2022-12-24 19:51:32 +0300

    added new class reverse iterator

---
+++ HomeworkIterator/src/ReverseIterator.java
@@ -0,0 +1,14 @@
+import java.util.Iterator;
+
+public class ReverseIterator implements Iterator<Students> {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Students next() {
+        return null;
+    }
+}
