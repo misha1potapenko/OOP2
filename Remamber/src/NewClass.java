7c991e1f7ccbe608da5d2f4312af9a329b479c84
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-09 21:17:28 +0300

    added constructor new class

--- Remamber/src/NewClass.java
+++ Remamber/src/NewClass.java
@@ -3,5 +3,30 @@ public class NewClass {
     private Integer age;
     private double avrMark;
 
+    public NewClass(String name, Integer age, double avrMark) {
+        this.name = name;
+        this.age = age;
+        this.avrMark = avrMark;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public double getAvrMark() {
+        return avrMark;
+    }
+
+    @Override
+    public String toString() {
+        return "NewClass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", avrMark=" + avrMark +
+                '}';
+    }
 }
