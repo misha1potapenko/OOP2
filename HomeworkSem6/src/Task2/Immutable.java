a693235bcb32370aabc94926b737112e8f2a918d
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-20 21:55:07 +0300

    added implements metod in immutable

--- HomeworkSem6/src/Task2/Immutable.java
+++ HomeworkSem6/src/Task2/Immutable.java
@@ -1,4 +1,13 @@
 package Task2;
 
-public class Immutable {
+public class Immutable extends Collection {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public int get(int i) {
+        return 0;
+    }
 }
