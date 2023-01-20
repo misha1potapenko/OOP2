c58dbad65de49bfc8d2c26e4aa0d617a4cb786c5
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-20 21:57:36 +0300

    added implement metod in

--- HomeworkSem6/src/Task2/Mutable.java
+++ HomeworkSem6/src/Task2/Mutable.java
@@ -1,4 +1,13 @@
 package Task2;
 
-public class Mutable {
+public class Mutable extends Collection {
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public int get(int i) {
+        return 0;
+    }
 }
