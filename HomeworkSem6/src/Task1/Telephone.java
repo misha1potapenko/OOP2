166d0fe49089e98d10f9167467d9b6a019b44e1c
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-22 19:14:22 +0300

    added two method in telephone

--- HomeworkSem6/src/Task1/Telephone.java
+++ HomeworkSem6/src/Task1/Telephone.java
@@ -1,7 +1,14 @@
 package Task1;
 
 public class Telephone {
+    private boolean isOn;
     public static void main(String[] args) {
 
+
     }
+    public void turnOn () {isOn = true;
+        System.out.println("Telephone is on");}
+
+    public void turnOff () {isOn = false;
+        System.out.println("Telephone is off");}
 }
