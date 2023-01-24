9dfdc31697345f1ca3a6105d5c851228c7d14446
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-24 20:34:46 +0300

    added fild site in LegalEntity

--- FinalProjectOOP/src/LegalEntity.java
+++ FinalProjectOOP/src/LegalEntity.java
@@ -1,7 +1,12 @@
 import java.util.List;
 
 public class LegalEntity extends Counterparty{
-    public LegalEntity(String name, List<Integer> telephone, String address, String emailAddress, String addressVK, String addressTelegram) {
+    private String site;
+
+
+    public LegalEntity(String name, List<Integer> telephone, String address, String emailAddress, String addressVK,
+                       String addressTelegram, String site) {
         super(name, telephone, address, emailAddress, addressVK, addressTelegram);
+        this.site = site;
     }
 }
