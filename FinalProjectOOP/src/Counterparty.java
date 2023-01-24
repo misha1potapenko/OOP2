1192660ddf5634816c25a8034829f82754a21a15
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-24 20:26:34 +0300

    added new fild

---
+++ FinalProjectOOP/src/Counterparty.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public abstract class Counterparty {
+    private String name;
+    private List<Integer> telephone;
+    private String address;
+    private String emailAddress;
+    private String addressVK;
+    private String addressTelegram;
+
+}
