8718fa18554d76f4c0a6a7bee52f6fc197491981
misha1potapenko <misha1potapenko@yandex.ru>
2023-01-25 18:54:10 +0300

    added list counterparties

--- FinalProjectOOP/src/ListContacts.java
+++ FinalProjectOOP/src/ListContacts.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class ListContacts {
-    private List<LegalEntity> legalEntities;
-    private List<PhysicalPerson> physicalPeople;
+    private List<Counterparty> Counterparties;
+
 
 }
